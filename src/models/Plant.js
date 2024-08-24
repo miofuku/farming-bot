70fa1d472d1aaee82ce3ba63ff23aa6568671781
Bijun Li <bijunli86@gmail.com>
2024-08-24 23:53:44 +0200

    Update models

--- src/models/Plant.js
+++ src/models/Plant.js
@@ -6,14 +6,13 @@ function Plant(_node) {
   if (this.id) {
     this.id = this.id.toNumber();
   }
-  if (this.duration) {
-    this.duration = this.duration.toNumber();
+  // Convert any Neo4j Integer properties to JavaScript numbers
+  if (this.speciesCount) {
+    this.speciesCount = this.speciesCount.toNumber();
   }
-  if (this.votes) {
-    this.votes = this.votes.toNumber();
-  } else {
-    this.votes = 0;
-  }
+
+  // Initialize default values
+  this.rotationWeight = this.rotationWeight ? this.rotationWeight.toNumber() : 1.0;
 }
 
-module.exports = Movie;
+module.exports = Plant;
